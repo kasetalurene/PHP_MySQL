fe4c8222a7013aec4f3bdfcd16483e123ee12f43
renekasetalu <rene.kasetalu@gmail.com>
2020-03-08 14:11:24 +0200

    Küpsised ja sessioonid

---
+++ kypsised/cookies.php
@@ -0,0 +1,42 @@
+<?php
+//kontrollime kas küpsised on olemas
+//kui pole, siis tekitame
+//kui on, lisame väärtusele ühe juurde
+if (!isset($_COOKIE["nimi"])) {
+    setcookie("nimi", 1, time()+3600);
+    echo "See on sinu esimene kylastus";
+} else {
+    $kylastus = $_COOKIE["nimi"];
+    $kylastus++;
+    setcookie("nimi", $kylastus, time()+3600);
+    echo "See on sinu $kylastus kord<br>";
+//kustutamise osa
+//loome lingi, kus k=1
+    $sait = $_SERVER["PHP_SELF"];
+    echo "<a href=\"$sait?k=1\">Kustuta küpsised</a>";
+    //kui k=1, siis muudame küpsise tähtaega
+    //ja suuname esilehele tagasi
+    if (!empty($_GET['k'])) {
+        $k = $_GET['k'];
+        if ($k==1) {
+            echo "$k";
+            setcookie("nimi", $kylastus, time()-3600);
+            header("location: $sait");
+        }
+    }
+}
+
+?>
+
+<?php
+	//sessiooni alustamine
+	session_start();
+	//sessiooni muutuja ja selle väärtus
+	$_SESSION['nimi'] = 'rene';
+	//sessiooni 'nimi' väärtuse väljastamine
+	echo $_SESSION['nimi'];
+
+    session_destroy();
+?>
+
+

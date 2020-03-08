3fb073f88ff0af616aa0f8a384936a464dfb7b41
renekasetalu <rene.kasetalu@gmail.com>
2020-03-08 15:52:57 +0200

    Töötamine vormidega: WYSIWYG

---
+++ tootaminevormidega/katsetused.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WYSIWYG</title>
+    <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
+	<style type="text/css">
+    #sisu{
+    width: 700px;
+		}
+	</style>
+</head>
+<body>
+<div id="sisu">
+	<h1>Artikli lisamine</h1>
+
+	<form action="" method="post">
+        <textarea class="ckeditor" cols="20" name="uudis" id="uudis" rows="10">
+</textarea>
+			<br>
+			<input type="submit" value="Salvesta">
+	</form>
+</div>
+</body>
+
+</html>
